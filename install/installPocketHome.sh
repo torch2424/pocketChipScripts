7223fcecdc271cc342795ed03b8ea2c22abb3cb5
Aaron Turner <aaron@aaronthedev.com>
2016-12-07 01:15:51 -0800

    Added xinput calibrator to poket home

--- install/installPocketHome.sh
+++ install/installPocketHome.sh
@@ -4,6 +4,8 @@
 #https://bbs.nextthing.co/t/pocket-home-marshmallow-edition/6579
 #Keep this script up-to-date with their installation instructions
 install-pocket-home() {
+    #Install xinput calibrator, or pocket home will complain
+    sudo apt-get install -y xinput-calibrator
     wget -O install-pockethome http://bit.ly/29uWueR
     chmod +x install-pockethome
     ./install-pockethome
