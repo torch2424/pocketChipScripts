ae34ad2c95c1c63cf0e7384bc37142faa58326e6
Aaron Turner <aaron@aaronthedev.com>
2016-12-11 16:49:02 -0800

    Fixed incorrect quake 3 name

--- install/installQuake3.sh
+++ install/installQuake3.sh
@@ -2,7 +2,7 @@
 
 #Install Quake III
 #http://blog.nextthing.co/accelerate-your-3d-gaming-with-quake-iii-on-pocketc-h-i-p/
-install-quake() {
+install-quake-3() {
     git clone https://github.com/NextThingCo/ioquake3-gles.git
     cd ioquake3-gles
     ./build.sh
