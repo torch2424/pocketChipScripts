b607cf3812a903f4d683370ccb94f6d878733f79
Aaron Turner <torch2424@users.noreply.github.com>
2016-12-12 22:34:58 -0800

    Added missing -y for Doom Install

--- install/installDoom.sh
+++ install/installDoom.sh
@@ -3,7 +3,7 @@
 #Install Doom
 #http://blog.nextthing.co/customize-the-hell-out-of-your-pocketc-h-i-p-install-doom-give-it-an-icon-on-the-home-screen/
 install-doom() {
-    sudo apt install prboom doom-wad-shareware
+    sudo apt install -y prboom doom-wad-shareware
 }
 
 #Open with the command: prboom
